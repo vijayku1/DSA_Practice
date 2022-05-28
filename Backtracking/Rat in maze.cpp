e6d4c2a937c5e69ca0abf6d5e921f6159f8a137e
VIJAY KUMAR <87418015+vijayku1@users.noreply.github.com>
2022-05-28 19:26:00 +0530

    backtracking

--- Backtracking/Rat in maze.cpp
+++ Backtracking/Rat in maze.cpp
@@ -1,7 +1,12 @@
 /*
 https://practice.geeksforgeeks.org/problems/rat-in-a-maze-problem/1#
 
-Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be travel through it.
+Consider a rat placed at (0, 0) in a square matrix of order N * N. 
+It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach 
+from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). 
+Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell
+ in the matrix represents that rat can be travel through it.
+ 
 Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.
 
 Example 1:
